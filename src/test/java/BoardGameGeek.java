c3928041c56200e03535a3a856f25ba53489acc9
tomas.helmfridsson <tomas.helmfridsson@sj.se>
2025-02-18 10:03:45 +0100

    Selenium BoardGameGeek 1

--- src/test/java/BoardGameGeek.java
+++ src/test/java/BoardGameGeek.java
@@ -1,25 +1,25 @@
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BoardGameGeek {
 
     @Test
 
-    public void testGoogleSearch() throws InterruptedException {
-
-        // Optional. If not specified, WebDriver searches the PATH for chromedriver.
-        // System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+    public void testGoogleSearch(){
         WebDriver driver = new ChromeDriver();
-
         driver.get("http://www.boardgamegeek.com/");
-        driver.findElement(By.cssSelector(".fc-button-label")).click();
-
-
-        // driver.quit();
-
+        driver.findElement(By.className("fc-button-label")).click();
+        driver.findElement(By.cssSelector("input[name='searchTerm']")).sendKeys("Things in Rings", Keys.ENTER);
+        driver.findElement(By.cssSelector("a.primary")).click();
+        driver.findElement((By.cssSelector("[href=\"/boardgame/408547/things-in-rings/videos/all\"]"))).click();
+        assertEquals("Things in Rings | Board Game | BoardGameGeek",driver.getTitle());
+        driver.quit();
     }
 
 }
